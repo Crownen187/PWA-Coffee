ef4c4bca0648e23e2610e02807e62789b571b0de
Alex03k <alexander.kunkel2@stud.fra-uas.de>
2023-11-03 11:57:11 +0100

    check

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ if (navigator.serviceWorker) {
   });
 }
 
+// Funktion, um Benutzermedien zu erhalten
 function getUserMedia(options, successCallback, failureCallback) {
   var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -21,6 +22,7 @@ var theStream;
 var theRecorder;
 var recordedChunks = [];
 
+// Funktion, um die Video-Stream-Aufnahme zu starten
 function getStream() {
   if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
     !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
@@ -28,7 +30,7 @@ function getStream() {
     return;
   }
   
-  var constraints = {video: true, audio: true};
+  var constraints = { video: true, audio: true };
   getUserMedia(constraints, function (stream) {
     var mediaControl = document.querySelector('video');
     
@@ -42,7 +44,7 @@ function getStream() {
     
     theStream = stream;
     try {
-      recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
+      var recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     } catch (e) {
       console.error('Exception while creating MediaRecorder: ' + e);
       return;
@@ -56,17 +58,19 @@ function getStream() {
   });
 }
 
+// Funktion, um aufgezeichnete Daten zu verarbeiten
 function recorderOnDataAvailable(event) {
   if (event.data.size == 0) return;
   recordedChunks.push(event.data);
 }
 
+// Funktion zum Herunterladen der aufgezeichneten Daten
 function download() {
   console.log('Saving data');
   theRecorder.stop();
   theStream.getTracks()[0].stop();
 
-  var blob = new Blob(recordedChunks, {type: "video/webm"});
+  var blob = new Blob(recordedChunks, { type: "video/webm" });
   var url = (window.URL || window.webkitURL).createObjectURL(blob);
   var a = document.createElement("a");
   document.body.appendChild(a);
@@ -75,28 +79,24 @@ function download() {
   a.download = 'test.webm';
   a.click();
   
-  // setTimeout() here is needed for Firefox.
+  // setTimeout() hier ist notwendig für Firefox.
   setTimeout(function () {
       (window.URL || window.webkitURL).revokeObjectURL(url);
   }, 100); 
 }
 
+// Funktion zum Speichern der aufgezeichneten Daten im lokalen Speicher
 function cacheSave(){
   var blob = new Blob(recordedChunks, {type: "video/webm"});
   var url = (window.URL || window.webkitURL).createObjectURL(blob);
   window.localStorage.setItem('video', url);
-
 }
 
+// Funktion zum Laden der aufgezeichneten Daten aus dem lokalen Speicher
 function cacheLoad(){
   var url = window.localStorage.getItem('video');
-  //video abspielen
+  // Video abspielen
   var mediaControl = document.querySelector('video');
   mediaControl.src = url;
-  mediaControl.play('video');
+  mediaControl.play();
 }
-  
-  // setTimeout() here is needed for Firefox.
-  setTimeout(function () {
-      (window.URL || window.webkitURL).revokeObjectURL(url);
-  }, 100); 
